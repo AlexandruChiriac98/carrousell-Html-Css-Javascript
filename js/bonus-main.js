858cf10b882f0718235637128aba4ac866d78031
Alexandru Chiriac <112606678+AlexandruChiriac98@users.noreply.github.com>
2024-06-03 14:09:33 +0200

    puuuffffwuaaaahh

--- js/bonus-main.js
+++ js/bonus-main.js
@@ -5,9 +5,10 @@ const imagesList = [
     "img/04.webp",
     "img/05.webp",
 ];
+let currentImgIndex = [0];
 const imgSliderEl = document.getElementById("img-slider");
 const thumbSliderEl = document.getElementById("thumb-slider");
-const btnNextEl = document.getElementById("bnt-next");
+const btnNextEl = document.getElementById("btn-next");
 const btnPrevEl = document.getElementById("btn-prev");
 
 
@@ -18,7 +19,25 @@ for (let i = 0 ; i < imagesList.length ; i++){
 
     const imgEl = document.createElement("img");
     imgEl.src = currentImg;
-    thumbSliderEl.append(imgEl);
+    if ( i === 0 ) { 
+        imgEl.classList.add("my-active");
+        thumbSliderEl.append(imgEl);
+    } else {
+        thumbSliderEl.append(imgEl);
+    }
+}
+
+for (let i = 0 ; i < imagesList.length ; i++){
+    const currentImg = imagesList[i];
+
+    const imgEl = document.createElement("img");
+    imgEl.src = currentImg;
+    if ( i === 0 ) { 
+        imgEl.classList.add("my-active");
+        imgSliderEl.append(imgEl);
+    } else {
+        imgSliderEl.append(imgEl);
+    }
 }
 
 
@@ -28,4 +47,57 @@ for (let i = 0 ; i < imagesList.length ; i++){
 
 btnNextEl.addEventListener("click" , function() {
 
-})
+    /*************************THUMB SLIDER REMOVE ACTIVE B.UP************************/
+    const oldThumb = document.querySelector(`#thumb-slider :nth-child(${ currentImgIndex + 1})`);
+    oldThumb.classList.remove("my-active");
+
+
+
+    /********************************BIG IMAGE REMOVE ACTIVE B.UP********************************/
+    const oldImg = document.querySelector(`#img-slider :nth-child(${ currentImgIndex + 1})`);
+    oldImg.classList.remove("my-active");
+
+
+
+    currentImgIndex--;
+    
+    if (currentImgIndex == -1){
+        currentImgIndex = 4;
+    }
+
+    /*************THUMB ADD ACTIVE*************/
+    const thumb = document.querySelector(`#thumb-slider :nth-child(${ currentImgIndex + 1})`);
+    thumb.classList.add("my-active");
+
+    /**************BIG IMAGE ADD ACTIVE*******************/
+    const nowImg = document.querySelector(`#img-slider :nth-child(${ currentImgIndex + 1})`);
+    nowImg.classList.add("my-active");
+
+
+});
+
+btnPrevEl.addEventListener("click" , function() {
+
+    /*************************THUMB SLIDER REMOVE ACTIVE B.DOWN************************/
+    const oldThumb = document.querySelector(`#thumb-slider :nth-child(${ currentImgIndex + 1})`);
+    oldThumb.classList.remove("my-active");
+
+    /********************************BIG IMAGE REMOVE ACTIVE B.DOWN********************************/
+    const oldImg = document.querySelector(`#img-slider :nth-child(${ currentImgIndex + 1})`);
+    oldImg.classList.remove("my-active");
+
+    currentImgIndex++;
+
+    if (currentImgIndex == 5){
+        currentImgIndex = 0;
+    }
+
+    /**********THUMB ADD ACTIVE*********/
+    const thumb = document.querySelector(`#thumb-slider :nth-child(${ currentImgIndex + 1})`);
+    thumb.classList.add("my-active");
+
+    /**************BIG IMAGE ADD ACTIVE*******************/
+    const nowImg = document.querySelector(`#img-slider :nth-child(${ currentImgIndex + 1})`);
+    nowImg.classList.add("my-active");
+    
+});
